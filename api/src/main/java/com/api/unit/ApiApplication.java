441a2975c79908880231e19eb650dc9d7d359cf5
Manchan777 <freshguni@gmail.com>
2024-12-14 22:08:08 +0900

    /* 241214 master -> main 변경 - 최병권 */

--- api/src/main/java/com/api/unit/ApiApplication.java
+++ api/src/main/java/com/api/unit/ApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-        System.out.println("자 이제 시작이야 내꿈을");
+        System.out.println("자 이제 시작이야 내꿈을~");
     }
 }
 
